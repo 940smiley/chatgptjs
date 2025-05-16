ac5ff0f1a6cbb46e4b58f9f22638aefd91dfb665
JOSHUA CHENNAULT <116070578+940smiley@users.noreply.github.com>
2025-05-16 06:04:47 -0500

    Initial commit

---
+++ extension/popup/controller.js
@@ -0,0 +1,213 @@
+(async () => {
+
+    // Import JS resources
+    for (const resource of ['components/icons.js', 'lib/dom.js', 'lib/settings.js'])
+        await import(chrome.runtime.getURL(resource))
+
+    // Init ENV context
+    window.env = {
+        site: new URL((await chrome.tabs.query({ active: true, currentWindow: true }))[0].url)
+            .hostname.split('.').slice(-2, -1)[0] // extract 2nd-level domain
+    }
+
+    // Import DATA
+    ;({ app: window.app } = await chrome.storage.local.get('app'))
+
+    // Define FUNCTIONS
+
+    function createMenuEntry(entryData) {
+        const entry = {
+            div: dom.create.elem('div', {
+                id: entryData.key, class: 'menu-entry highlight-on-hover', title: entryData.helptip || '' }),
+            leftElem: dom.create.elem('div', { class: `menu-icon ${ entryData.type || '' }` }),
+            label: dom.create.elem('span')
+        }
+        entry.label.textContent = entryData.label
+        entry.div.append(entry.leftElem, entry.label)
+        if (entryData.type == 'toggle') { // add track to left, init knob pos
+            entry.leftElem.append(dom.create.elem('span', { class: 'track' }))
+            entry.leftElem.classList.toggle('on', settings.typeIsEnabled(entryData.key))
+        } else { // add symbol to left, append status to right
+            entry.leftElem.textContent = entryData.symbol || '⚙️' ; entry.label.style.flexGrow = 1
+            if (entryData.status) entry.label.textContent += ` — ${entryData.status}`
+            if (entryData.type == 'link') {
+                entry.label.after(entry.rightElem = dom.create.elem('div', { class: 'menu-right-elem' }))
+                entry.rightElem.append(icons.create({ key: 'open', size: 18, fill: 'black' }))
+            }
+        }
+        if (entryData.type == 'category')
+            entry.div.append(icons.create({ key: 'caretDown', size: 11, class: 'menu-caret menu-right-elem' }))
+        entry.div.onclick = () => {
+            if (entryData.type == 'category') toggleCategorySettingsVisiblity(entryData.key)
+            else if (entryData.type == 'toggle') {
+                entry.leftElem.classList.toggle('on')
+                settings.save(entryData.key, !config[entryData.key]) ; sync.configToUI({ updatedKey: entryData.key })
+                requestAnimationFrame(() => notify(`${entryData.label} ${chrome.i18n.getMessage(`state_${
+                    settings.typeIsEnabled(entryData.key) ? 'on' : 'off' }`).toUpperCase()}`))
+            } else if (entryData.type == 'link') { open(entryData.url) ; close() }
+        }
+        return entry.div
+    }
+
+    function notify(msg, pos = 'bottom-right') { sendMsgToActiveTab('notify', { msg, pos }) }
+
+    async function sendMsgToActiveTab(action, options) {
+        const activeTabID = (await chrome.tabs.query({ active: true, currentWindow: true }))[0].id
+        return await chrome.tabs.sendMessage(activeTabID, { action, options })
+    }
+
+    const sync = {
+        fade() {
+
+            // Toolbar icon
+            chrome.action.setIcon({ path: Object.fromEntries(
+                Object.keys(chrome.runtime.getManifest().icons).map(dimension =>
+                    [dimension, `../icons/${ config.extensionDisabled ? 'faded/' : '' }icon${dimension}.png`]
+            ))})
+
+            // Menu elems
+            document.querySelectorAll('.logo, .menu-title, .menu-entry').forEach((elem, idx) => {
+                if (elem.id && ( elem.matches(`#${elem.id}:has(> div.link)`) || elem.id == 'aboutEntry' ))
+                    return // never disable link/About entries
+                elem.style.transition = config.extensionDisabled ? '' : 'opacity 0.15s ease-in'
+                setTimeout(() => elem.classList.toggle('disabled', config.extensionDisabled),
+                    config.extensionDisabled ? 0 : idx *10) // fade-out abruptly, fade-in staggered
+            })
+        },
+
+        configToUI(options) { return sendMsgToActiveTab('syncConfigToUI', options) }
+    }
+
+    function toggleCategorySettingsVisiblity(category, { transitions = true, action } = {}) {
+        const transitionDuration = 350, // ms
+              categoryDiv = document.getElementById(category),
+              caret = categoryDiv.querySelector('.menu-caret'),
+              catChildrenDiv = categoryDiv.nextSibling,
+              catChild = catChildrenDiv.querySelectorAll('.menu-entry')
+        if (action != 'hide' && dom.get.computedHeight(catChildrenDiv) == 0) { // show category settings
+            categoryDiv.classList.toggle('expanded', true)
+            Object.assign(catChildrenDiv.style, { height: `${dom.get.computedHeight(catChild)}px`,
+                transition: transitions ? 'height 0.25s' : '' })
+            Object.assign(caret.style, { // point it down
+                transform: 'rotate(0deg)', transition: transitions ? 'transform 0.15s ease-out' : '' })
+            catChild.forEach(row => { // reset styles to support continuous transition on rapid show/hide
+                row.style.transition = 'none' ; row.style.opacity = 0 })
+            catChildrenDiv.offsetHeight // force reflow to insta-apply reset
+            catChild.forEach((row, idx) => { // fade-in staggered
+                if (transitions) row.style.transition = `opacity ${ transitionDuration /1000 }s ease-in-out`
+                setTimeout(() => row.style.opacity = 1, transitions ? idx * transitionDuration /10 : 0)
+            })
+            document.querySelectorAll(`.menu-entry:has(.menu-caret):not(#${category})`).forEach(otherCategoryDiv =>
+                toggleCategorySettingsVisiblity(otherCategoryDiv.id, { action: 'hide' }))
+        } else { // hide category settings
+            categoryDiv.classList.toggle('expanded', false)
+            Object.assign(catChildrenDiv.style, { height: 0, transition: '' })
+            Object.assign(caret.style, { transform: 'rotate(-90deg)', transition: '' }) // point it right
+        }
+    }
+
+    // Run MAIN routine
+
+    // Init MASTER TOGGLE
+    const masterToggle = {
+        div: document.querySelector('.master-toggle'),
+        switch: dom.create.elem('div', { class: 'toggle menu-icon highlight-on-hover' }),
+        track: dom.create.elem('span', { class: 'track' })
+    }
+    masterToggle.div.append(masterToggle.switch) ; masterToggle.switch.append(masterToggle.track)
+    await settings.load('extensionDisabled') ; masterToggle.switch.classList.toggle('on', !config.extensionDisabled)
+    masterToggle.div.onclick = () => {
+        env.extensionWasDisabled = config.extensionDisabled
+        masterToggle.switch.classList.toggle('on') ; settings.save('extensionDisabled', !config.extensionDisabled)
+        Object.keys(sync).forEach(key => sync[key]()) // sync fade + storage to UI
+        notify(`${app.name} ${ this.checked ? 'ON' : 'OFF' }`)
+    }
+
+    // Create CHILD menu entries on chatgpt.com
+    const footer = document.querySelector('footer')
+    if (env.site == 'chatgpt') {
+        await settings.load(Object.keys(settings.controls))
+        const menuEntriesDiv = dom.create.elem('div') ; footer.before(menuEntriesDiv)
+
+        // Group controls by category
+        const categorizedCtrls = {}
+        Object.entries(settings.controls).forEach(([key, ctrl]) =>
+            ( categorizedCtrls[ctrl.category || 'general'] ??= {} )[key] = { ...ctrl, key: key })
+
+        // Create/append general controls
+        Object.values(categorizedCtrls.general || {}).forEach(ctrl => menuEntriesDiv.append(createMenuEntry(ctrl)))
+
+        // Create/append categorized controls
+        Object.entries(categorizedCtrls).forEach(([category, ctrls]) => {
+            if (category == 'general') return
+            const catData = { ...settings.categories[category], key: category, type: 'category' },
+                  catChildrenDiv = dom.create.elem('div', { class: 'categorized-entries' })
+            if (catData.color) // color the stripe
+                catChildrenDiv.style.borderImage = `linear-gradient(transparent, #${catData.color}) 30 100%`
+            menuEntriesDiv.append(createMenuEntry(catData), catChildrenDiv)
+            Object.values(ctrls).forEach(ctrl => catChildrenDiv.append(createMenuEntry(ctrl)))
+        })
+    }
+
+    // Create/append ABOUT entry
+    const aboutEntry = {
+        div: createMenuEntry({ key: 'aboutEntry', symbol: '💡', label: 'About...', helptip: `About ${app.name}` }),
+        ticker: {
+            textGap: '&emsp;&emsp;&emsp;',
+            span: dom.create.elem('span', { class: 'ticker' }), innerDiv: dom.create.elem('div')
+        }
+    }
+    aboutEntry.div.querySelector('div.menu-icon').style.paddingLeft = '10px'
+    aboutEntry.div.querySelector('span').style.paddingLeft = '2.5px'
+    aboutEntry.ticker.content = `Version: <span class="ticker-em">v${ app.version + aboutEntry.ticker.textGap }</span>`
+                         + `Powered by <span class="ticker-em">chatgpt.js</span>${aboutEntry.ticker.textGap}`
+    for (let i = 0 ; i < 7 ; i++) aboutEntry.ticker.content += aboutEntry.ticker.content // make long af
+    aboutEntry.ticker.innerDiv.innerHTML = aboutEntry.ticker.content
+    aboutEntry.ticker.span.append(aboutEntry.ticker.innerDiv)
+    aboutEntry.div.append(aboutEntry.ticker.span) ; footer.before(aboutEntry.div)
+    aboutEntry.div.onclick = () => { chrome.runtime.sendMessage({ action: 'showAbout' }) ; close() }
+
+    // Create/append CHATGPT entry
+    const activeTabURL = (await chrome.tabs.query({ active: true, currentWindow: true }))[0].url,
+          chatgptURL = chrome.runtime.getManifest().content_scripts[0].matches.map(url => url.replace(/\/\*$/, ''))
+    if (!activeTabURL.includes(chatgptURL)) footer.before(createMenuEntry({
+        key: 'chatgptEntry', type: 'link', symbol: '💬', label: 'Open ChatGPT', url: chatgptURL, helptip: chatgptURL }))
+
+    // Create/append LATEST CHANGES entry
+    const latestChangesURL = `${app.urls.github}/commits`
+    footer.before(createMenuEntry({
+        key: 'latestChangesEntry', type: 'link', symbol: '🚀',
+        label: 'Latest Changes...', url: latestChangesURL, helptip: latestChangesURL
+    }))
+
+    // Init FOOTER
+    const footerElems = { // left-to-right
+        chatgptjs: { logo: footer.querySelector('.chatgptjs-logo') },
+        about: { span: footer.querySelector('.about-span') },
+        moreExt: { span: footer.querySelector('.more-ext-span') }
+    }
+    footerElems.chatgptjs.logo.src = 'https://cdn.jsdelivr.net/gh/KudoAI/chatgpt.js@745f0ca'
+                                   + '/assets/images/badges/powered-by-chatgpt.js.png'
+    footerElems.chatgptjs.logo.onclick = () => { open(app.urls.chatgptjs) ; close() }
+    footerElems.about.span.title = `About ${app.name}`
+    footerElems.about.span.append(icons.create({ key: 'questionMark', width: 15, height: 13 }))
+    footerElems.about.span.onclick = () => { chrome.runtime.sendMessage({ action: 'showAbout' }) ; close() }
+    footerElems.moreExt.span.title = 'More AI Extensions'
+    footerElems.moreExt.span.append(icons.create({ key: 'plus' }))
+    footerElems.moreExt.span.onclick = () => { open(app.urls.relatedExtensions) ; close() }
+
+    // AUTO-EXPAND categories
+    document.querySelectorAll('.menu-entry:has(.menu-caret)').forEach(categoryDiv => {
+        if (settings.categories[categoryDiv.id]?.autoExpand)
+            toggleCategorySettingsVisiblity(categoryDiv.id, { transitions: false })
+    })
+
+    // Remove LOADING SPINNER after imgs load
+    Promise.all([...document.querySelectorAll('img')].map(img =>
+        img.complete ? Promise.resolve() : new Promise(resolve => img.onload = resolve)
+    )).then(() => {
+        document.querySelectorAll('[class^=loading]').forEach(elem => elem.remove())
+        sync.fade() // based on master toggle state
+    })
+
+})()
---
+++ extension/service-worker.js
@@ -0,0 +1,40 @@
+const chatgptURL = 'https://chatgpt.com'
+
+// Init APP data
+chrome.storage.local.set({ app: {
+    name: chrome.runtime.getManifest().name,
+    version: chrome.runtime.getManifest().version, symbol: '🤖', cssPrefix: 'chatgpt-extension',
+    author: { name: 'KudoAI', url: 'https://kudoai.com' },
+    urls: {
+        chatgptjs: 'https://chatgptjs.org',
+        contributors: 'https://docs.chatgptjs.org/#-contributors',
+        gitHub: 'https://github.com/KudoAI/chatgpt.js-chrome-starter',
+        relatedExtensions: 'https://aiwebextensions.com',
+        support: 'https://github.com/KudoAI/chatgpt.js-chrome-starter/issues'
+    }
+}}) // save to Chrome storage
+
+// Launch CHATGPT on install
+chrome.runtime.onInstalled.addListener(details => {
+    if (details.reason == 'install') // to exclude updates
+        chrome.tabs.create({ url: chatgptURL })
+})
+
+// Sync SETTINGS to activated tabs
+chrome.tabs.onActivated.addListener(activeInfo =>
+    chrome.tabs.sendMessage(activeInfo.tabId, { action: 'syncConfigToUI' }))
+
+// Show ABOUT modal on ChatGPT when toolbar button clicked
+chrome.runtime.onMessage.addListener(async req => {
+    if (req.action == 'showAbout') {
+        const [activeTab] = await chrome.tabs.query({ active: true, currentWindow: true })
+        const chatgptTab = new URL(activeTab.url).hostname == 'chatgpt.com' ? activeTab
+            : await chrome.tabs.create({ url: chatgptURL })
+        if (activeTab != chatgptTab) await new Promise(resolve => // after new tab loads
+            chrome.tabs.onUpdated.addListener(function loadedListener(tabId, changeInfo) {
+                if (tabId == chatgptTab.id && changeInfo.status == 'complete') {
+                    chrome.tabs.onUpdated.removeListener(loadedListener) ; setTimeout(resolve, 500)
+        }}))
+        chrome.tabs.sendMessage(chatgptTab.id, { action: 'showAbout' })
+    }
+})
